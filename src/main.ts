5c777674f47a8e37c38387827f84853c22686b5b
Mara Kim <mara@autochthe.net>
2019-05-16 20:52:52 -0400

    Basic requirements

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import {createCanvas, Canvas, CanvasRenderingContext2D} from "canvas";
+import fs from "fs";
+import pdfjsLib from "pdfjs-dist";
+
+function main() {
+    console.log("hello")
+    console.log(pdfjsLib)
+}
+
+export default main;
